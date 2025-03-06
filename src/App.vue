7b38264a668db49ec67b6ddf120eca57962cc7ce
ikuratugas <ikrartugas@gmail.com>
2025-03-06 22:18:01 +0800

    pertemuan 2 coba yang kedua kali

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
+import { ref } from 'vue';
+
+  let nama = ref("ikrar aprianto")
+
+  function tampil(){
+    console.log("tampilkan saja dulu")
+  }
+
 </script>
 
 <template>
   <h1>Belajar ulang lagi, suka pemprograman</h1>
   <main>
-    <h2>coba saja dulu</h2>
+    <h2>coba saja dulu {{ nama }}</h2>
+    <br>
+    <button @click="tampil">{{ nama }}</button>
   </main>
 </template>
 
