b4d71e6622cdd8c5e08edb1b937fef5681872d0f
ikuratugas <ikrartugas@gmail.com>
2025-03-09 19:46:57 +0800

    menampilkan salam setelah input text dan menekan button

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,10 @@
 import { ref } from 'vue';
   const inputNama = ref("")
 
-<<<<<<< HEAD
-  const nama = ref(false)
-
-  const listNama = ref([
-    "ikrar",
-    "tanti",
-    "selvi",
-  ])
-
-  const friends = ref(
-    [
-      "adam",
-      "sarif",
-      "asrul",
-      "almin",
-      "vito",
-    ]
-  )
-
-  function tampil(){
-    nama.value = !nama.value
-  }
-=======
   const tampilkan = ref(false)
 
   function tampil(){
     tampilkan.value = true
->>>>>>> coba
 
     setTimeout(function(){
       tampilkan.value = false
@@ -41,8 +17,6 @@ import { ref } from 'vue';
 <template>
   <h1>Belajar ulang lagi, suka pemprograman</h1>
   <main>
-<<<<<<< HEAD
-=======
     <input v-model="inputNama" type="text" name="" id="">
     <br>
     <button @click="tampil">Tampilkan nama</button>
@@ -53,26 +27,4 @@ import { ref } from 'vue';
 
   </main>
 </template>
->>>>>>> coba
 
-    <h2 v-if="nama === true">Ini jadi Benar</h2>
-    <h2 v-else>Ini jadi salah</h2>
-
-    <h3 v-if="nama === true">
-      <ul v-for="l in listNama">
-        <li>nama : {{ l }}</li>
-      </ul>
-    </h3>
-
-    <h2>List Friends</h2>
-    <ul v-for="(item, i) in friends">
-      <li>no. {{ i+1 }} - {{ item }}</li>
-    </ul>
-
-    <br>
-    <button v-on:click="tampil">Ubah Nilai</button>
-    <!-- // bisa juga begini -->
-    <!-- <button v-on:click="tampil" >{{ nama }}</button> -->
-
-  </main>
-</template>
