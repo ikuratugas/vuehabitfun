77f65ddd0c9ad322f88e15c9eef3bbab03c94d4b
ikuratugas <ikrartugas@gmail.com>
2025-03-12 23:51:47 +0800

    coba ini saja dulu, karena mau berganti hari.. namun aku lanjut begadang untuk coding ini

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { ref } from 'vue';
-
-  const nilai = ref(0);
-  
-
-  function tambah(){
-    nilai.value+=1
-  }
-
-  function kurang(){
-    nilai.value-=1
-  }
 </script>
 <template>
   <main>
-    <h1>Mencoba membuat counter tanpa melihat sumber lain, just coding</h1>
-    <button @click="tambah"> Tambah </button> 
+    <h1>Hari akan berganti, push dikit aja ya..</h1>
+    <button @click="tambah"> Tambah </button>
     <p>{{ nilai }}</p>
-    <button @click="kurang"> Tambah </button> 
+    <button @click="kurang"> Tambah </button>
   </main>
 </template>
 
