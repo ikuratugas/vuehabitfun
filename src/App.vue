0495298f27054c086b074e88c1682ca96bdca085
ikuratugas <ikrartugas@gmail.com>
2025-03-16 14:39:36 +0800

    kembali lagi ke vue lagi, setelah belajar struct golang

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup>
+import { ref } from 'vue';
+  const listNama = ref(
+    [
+      "ikrar",
+      "anto",
+      "rian",
+    ]
+  )
+
+  const tampil = ref(false)
+
+  function clickTampil(){
+    tampil.value = !tampil.value
+  }
+
+  console.log("coba saja dulu ini")
 </script>
 <template>
   <main>
-    <h1>Hari akan berganti, push dikit aja ya..</h1>
-    <button @click="tambah"> Tambah </button>
-    <p>{{ nilai }}</p>
-    <button @click="kurang"> Tambah </button>
+    <h1>Belajar List tanpa lihat source apapun</h1>
+    <button @click="clickTampil">Tampilkan Nama-nama yang tersedia</button>
+    <ul v-for="val, i in listNama  " v-if="tampil">
+      <li>no. {{ i+1 }} adalah {{ val }}</li>
+    </ul>
   </main>
 </template>
 
