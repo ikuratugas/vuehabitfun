dcf9d63c198647fff29b6713be32c64c0358d4be
ikuratugas <ikrartugas@gmail.com>
2025-03-22 23:23:03 +0800

    not dignity by gipity and i actually can do it.  yeah right it's take a lot of time, but for me is making coding fun again. i'm literally really want code more and is totally fun to doing it.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
   let keyid = 0
   const listNama = ref([
@@ -7,8 +7,33 @@ import { ref } from 'vue';
     {id: keyid++, nama : "sensi farchan", centang: false},
     {id: keyid++, nama : "senpai rahmat fahmi", centang: false},
 	])
-  // TODO: buat input tambah data
+
   const tampungText = ref("")
+  const textDiIsiKosong = ref(false)
+  const tampilkanSemuDatLisNama = ref(true)
+
+  function tambahData(){
+    if(tampungText.value === ""){
+      textDiIsiKosong.value = true
+      return null
+    }
+    listNama.value.push({id: keyid++, nama:tampungText.value, centang: false})
+    tampungText.value = ""
+  }
+
+  let dataFilterListNama = computed(() => {
+    return tampilkanSemuDatLisNama.value ?
+      listNama.value.filter((t) => t.centang === false):
+      listNama.value;
+  })
+
+  function merubahNilai(){
+    tampilkanSemuDatLisNama.value = !tampilkanSemuDatLisNama.value
+  }
+  
+  function hapusData(i){
+    listNama.value = listNama.value.filter((item)=> item.id !== i)
+  }
 
 </script>
 
@@ -17,16 +42,20 @@ import { ref } from 'vue';
   <main>
     <h1>Coba membuat CRUD</h1>
     <input type="text" v-model="tampungText" placeholder="Masukkan Nama">
-
+    <button v-on:click="tambahData">tambah data</button>
 
 	<ul>
-		<li v-for="item in listNama" :key="item.id">
+		<li v-for="item in dataFilterListNama" :key="item.id">
       <input type="checkbox" v-model="item.centang">
       <span :class="{tercentang: item.centang}">
         {{ item.nama }}
       </span>
+      <button v-on:click="hapusData(item.id)">X</button>
 		</li>
 	</ul>
+  <button v-on:click="merubahNilai">
+    {{ tampilkanSemuDatLisNama? "Tampilkan semua" : "Tampilkan Belum Tercentang" }}
+  </button>
   </main>
 
 </template>
